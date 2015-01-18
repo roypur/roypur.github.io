d2f2e91c568abe0edd33e4c9c6ece7087886d4da
Roy Olav Purser <admin@royolav.net>
2015-01-18 19:27:40 +0100

    js

--- test.js
+++ test.js
@@ -13,7 +13,7 @@ function addSpace(){
         var height = window.innerHeight; //available space
         }
         
-    alert(height + "x" + window.innerHeight + "x" + window.innerWidth + "x" +  window.outerWidth + "x" + window.outerHeight);
+    document.getElementById('space').innerHTML = height + "x" + window.innerHeight + "x" + window.innerWidth + "x" +  window.outerWidth + "x" + window.outerHeight;
         
         
         
