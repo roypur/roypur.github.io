a0435973d6186e4cd34937be6db413dae5879bdc
Roy Olav Purser <admin@royolav.net>
2015-01-18 18:27:24 +0100

    js

--- test.js
+++ test.js
@@ -1,23 +1,26 @@
 function addSpace(){
     
-    var height = window.innerHeight; //available space
+    if(typeof(window.orientation) == "undefined" || typeof(height) == "undefined"){
+
+        var height = window.innerHeight; //available space
     
-    document.getElementById('space').style.height = height;
+        document.getElementById('space').style.height = height;
     
-    var scroll = document.body.scrollHeight;
+        var scroll = document.body.scrollHeight;
     
-    var content = scroll - height;
+        var content = scroll - height;
     
-    var space = height-content; //size of space div
+        var space = height-content; //size of space div
     
-    if(space>100){
-        document.getElementById('space').style.height = space;
-        document.body.style.overflowY = 'hidden';
+        if(space>100){
+            document.getElementById('space').style.height = space;
+            document.body.style.overflowY = 'hidden';
+            }
+        else{
+            document.getElementById('space').style.height = 100;
+            document.body.style.overflowY = 'scroll';
+            }
         }
-    else{
-        document.getElementById('space').style.height = 100;
-        document.body.style.overflowY = 'scroll';
-        }
-}
+    }
 window.onload=addSpace;
 window.onresize=addSpace;
