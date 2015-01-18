a349bcfb0bba2bfa22e82179295209d3491f734b
Roy Olav Purser <admin@royolav.net>
2015-01-18 22:52:54 +0100

    toutch

--- test.js
+++ test.js
@@ -1,34 +1,31 @@
 function addSpace(){
     
     var height = window.innerHeight; //available space
-
+    
+    if(typeof(window.orientation) != Number || document.body.dataset.run != "false"){
+        document.body.dataset.run = "false";
         
     document.getElementById('space').innerHTML = window.innerHeight + "x" + window.innerWidth + "x" + document.body.scrollHeight;
         
-        
-        
-        
+    document.getElementById('space').style.height = height;
     
+    var scroll = document.body.scrollHeight;
+    
+    var content = scroll - height;
+    
+    var space = height-content; //size of space div
+    
+    if(space>100){
+        document.getElementById('space').style.height = space;
+        document.body.style.overflowY = 'hidden';
+        }
+    else{
+        document.getElementById('space').style.height = 100;
+        document.body.style.overflowY = 'scroll';
+        }
+            
+    } //end run if
+}
 
-    
-        document.getElementById('space').style.height = height;
-    
-        var scroll = document.body.scrollHeight;
-    
-        var content = scroll - height;
-    
-        var space = height-content; //size of space div
-    
-        if(space>100){
-            document.getElementById('space').style.height = space;
-            document.body.style.overflowY = 'hidden';
-            }
-        else{
-            document.getElementById('space').style.height = 100;
-            document.body.style.overflowY = 'scroll';
-            }
-
-    }
 window.onload=addSpace;
 window.onresize=addSpace;
-window.ontouchend=addSpace;
