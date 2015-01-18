0287947c14d90c06ce221c1387975d31fdfc4d10
Roy Olav Purser <admin@royolav.net>
2015-01-18 23:11:52 +0100

    toutch

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ function addSpace(){
     
     var height = window.innerHeight; //available space
     
-    if((typeof(window.orientation) != Number) || (document.body.dataset.run != "false")){
+    if((typeof(window.orientation) != "number") || (document.body.dataset.run != "false")){
         document.body.dataset.run = "false";
         
     document.getElementById('space').innerHTML = window.innerHeight + "x" + window.orientation + "x" + document.body.scrollHeight + "x" + document.body.dataset.run;
