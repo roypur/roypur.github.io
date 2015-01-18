28b619ba85dff2c1d7d4e70ad56c2ecfd1eb564c
Roy Olav Purser <admin@royolav.net>
2015-01-18 15:21:00 +0100

    test

---
+++ test.js
@@ -0,0 +1,24 @@
+function addSpace(){
+    var height = window.innerHeight; //available space
+    
+    document.getElementById('space').style.height = height;
+    
+    var scroll = document.body.scrollHeight;
+    
+    var content = scroll - height;
+    
+    var space = height-content; //size of space div
+    
+    document.getElementById('space').innerHTML = "scroll" + scroll + "<br><br>height" + height;
+    
+    if(space>100){
+        document.getElementById('space').style.height = space;
+        document.body.style.overflowY = 'hidden';
+        }
+    else{
+        document.getElementById('space').style.height = 100;
+        document.body.style.overflowY = 'scroll';
+        }
+}
+window.onload=addSpace;
+window.onresize=addSpace;
