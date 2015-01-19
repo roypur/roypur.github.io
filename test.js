b3a002cfd3097a0d8afdc505fce9c8610a221b3a
Roy Olav Purser <admin@royolav.net>
2015-01-19 14:55:24 +0100

    Revert "scroll"
    This reverts commit 75f67347ac27ca7481e2a8e52fdaa92ec731557c.

--- test.js
+++ test.js
@@ -1,14 +1,6 @@
 function addSpace(){
-    
-    var last = document.body.dataset.height;
-    
-    if(typeof(last) == "undefined" || window.innerHeight > last*1){
-        document.body.dataset.height = window.innerHeight;
-        }
-        
-    document.getElementById('space').innerHTML = window.innerHeight + "x" + last;
-    
-    document.body.dataset.height = 
+
+    document.getElementById('space').innerHTML = window.innerHeight + "x" + document.body.scrollHeight + "x" + document.getElementById('bottom').offsetTop;
     
     document.getElementById('space').style.height = 0;
     
