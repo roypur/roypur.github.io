59b256912636f5195db8cea18d4ef62c58904067
Roy Olav Purser <admin@royolav.net>
2015-01-19 13:28:21 +0100

    chrome fix

--- test.js
+++ test.js
@@ -2,31 +2,32 @@ function addSpace(){
     
     var last = document.body.dataset.height;
     
-    if(typeof(last) == "undefined" || window.innerHeight > last*1){
+    if(typeof(last) == "undefined" || window.innerHeight > last*1 || typeof(window.orientation) == "undefined"){
         document.body.dataset.height = window.innerHeight;
-        }
         
-    document.getElementById('space').innerHTML = window.innerHeight + "x" + last;
+        
+        document.getElementById('space').innerHTML = window.innerHeight + "x" + last;
     
-    document.getElementById('space').style.height = 0;
+        document.getElementById('space').style.height = 0;
     
-    var offset = document.getElementById('bottom').offsetTop;
+        var offset = document.getElementById('bottom').offsetTop;
     
-    var height = window.innerHeight; //available space
+        var height = window.innerHeight; //available space
 
-    var bottom = document.getElementById('bottom').offsetHeight;
+        var bottom = document.getElementById('bottom').offsetHeight;
     
-    var space = height - offset - bottom;
+        var space = height - offset - bottom;
     
-    if(space>100){
-        document.getElementById('space').style.height = space;
-        document.body.style.overflowY = 'hidden';
+        if(space>100){
+            document.getElementById('space').style.height = space;
+            document.body.style.overflowY = 'hidden';
+            }
+        else{
+            document.getElementById('space').style.height = 100;
+            document.body.style.overflowY = 'scroll';
+            }
         }
-    else{
-        document.getElementById('space').style.height = 100;
-        document.body.style.overflowY = 'scroll';
-        }
-}
+    }
 
 if(typeof(window.orientation) == "undefined"){
     window.onresize=addSpace;
