ea0012824a78f6aafa89eafeb0677587ad76a3c1
Roy Olav Purser <admin@royolav.net>
2015-01-19 14:03:23 +0100

    chrome fix

--- test.js
+++ test.js
@@ -26,8 +26,6 @@ function addSpace(){
 if(typeof(window.orientation) == "undefined"){
     window.onresize=addSpace;
     }
-else{
-    window.onscroll=addSpace;    
-    }
+
 window.onload=addSpace;
 
