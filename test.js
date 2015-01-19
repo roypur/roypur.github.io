0d4f0042db15c902e7f554bdba6164543480cf39
Roy Olav Purser <admin@royolav.net>
2015-01-19 14:55:23 +0100

    Revert "chrome fix"
    This reverts commit 59b256912636f5195db8cea18d4ef62c58904067.

--- test.js
+++ test.js
@@ -2,32 +2,31 @@ function addSpace(){
     
     var last = document.body.dataset.height;
     
-    if(typeof(last) == "undefined" || window.innerHeight > last*1 || typeof(window.orientation) == "undefined"){
+    if(typeof(last) == "undefined" || window.innerHeight > last*1){
         document.body.dataset.height = window.innerHeight;
-        
-        
-        document.getElementById('space').innerHTML = window.innerHeight + "x" + last;
-    
-        document.getElementById('space').style.height = 0;
-    
-        var offset = document.getElementById('bottom').offsetTop;
-    
-        var height = window.innerHeight; //available space
-
-        var bottom = document.getElementById('bottom').offsetHeight;
-    
-        var space = height - offset - bottom;
-    
-        if(space>100){
-            document.getElementById('space').style.height = space;
-            document.body.style.overflowY = 'hidden';
-            }
-        else{
-            document.getElementById('space').style.height = 100;
-            document.body.style.overflowY = 'scroll';
-            }
         }
-    }
+        
+    document.getElementById('space').innerHTML = window.innerHeight + "x" + last;
+    
+    document.getElementById('space').style.height = 0;
+    
+    var offset = document.getElementById('bottom').offsetTop;
+    
+    var height = window.innerHeight; //available space
+
+    var bottom = document.getElementById('bottom').offsetHeight;
+    
+    var space = height - offset - bottom;
+    
+    if(space>100){
+        document.getElementById('space').style.height = space;
+        document.body.style.overflowY = 'hidden';
+        }
+    else{
+        document.getElementById('space').style.height = 100;
+        document.body.style.overflowY = 'scroll';
+        }
+}
 
 if(typeof(window.orientation) == "undefined"){
     window.onresize=addSpace;
