a47fb3f133920bedd6795aa079c5900df9f28816
Roy Olav Purser <admin@royolav.net>
2015-01-19 14:55:19 +0100

    Revert "chrome fix"
    This reverts commit aaa7bcd6d31abb9b1a60df7819b21c0c70b76a70.

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ function addSpace(){
     
         var space = height - offset - bottom;
             
-        if(typeof(window.orientation) != "undefined" && space>0){
+        if(typeof(window.orientation) != "undefined"){
             space += 300;
             }
             
